dcb68ae54427a59608ade82fe1e6fcc1c5540c81
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-08-14 17:30:40 +0200

    feat: add function show menu

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+class Manager:
+    def __init__(self):
+        self.show_menu()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def show_menu(self):
+        menu = """
+        1. Encrypt word
+        5. Exit
+        """
+        print(menu)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    init = Manager()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
