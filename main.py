2a80a2bcddd6048538a1a1eedc4fdad8ecfdfa92
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-08-14 17:48:30 +0200

    feat: add docstring and typing

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 class Manager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.show_menu()
         self.get_and_execute_choice()
 
-    def show_menu(self):
+    def show_menu(self) -> None:
+        """ Function print menu of potential options"""
+
         menu = """
         5. Exit
         """
         print(menu)
 
-    def get_and_execute_choice(self):
+    def get_and_execute_choice(self) -> None:
+        """Function get from user information which operation he is interested and execute it
+
+        In case of choice the value out of scope show message
+
+        Work in loop until user chose option exit()
+        """
         user_choice = int(input("Choose what you want do: "))
         choices = {
             5: exit()
