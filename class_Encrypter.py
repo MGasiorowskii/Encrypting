8e041539f5028e6d8d335d7945ad2c0baede46cf
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-08-15 19:29:10 +0200

    feat: add new method get_key

--- class_Encrypter.py
+++ class_Encrypter.py
@@ -3,8 +3,14 @@ class Encrypter:
     def __init__(self) -> None:
         self.translated_sentence = ""
         self.sentence = self.get_string_to_encrypt().upper()
+        self.key = self.get_key()
 
     def get_string_to_encrypt(self) -> str:
         """Get form user string to encrypting"""
         return input("Input sentence to encrypting: ")
+
+    def get_key(self) -> int:
+        """Get from user key to encrypting"""
+        return int(input("Input key to encrypting: "))
+
 
