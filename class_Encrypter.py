810ccb3ea65f2f82316259b1fcc09218ead84b8a
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-08-15 19:26:38 +0200

    feat: add ne method get_string_to_encrypt

--- class_Encrypter.py
+++ class_Encrypter.py
@@ -1,6 +1,10 @@
 class Encrypter:
     """Represents a class used to encrypt a file using the Caesar cipher  mod47 or mod13"""
-    def __init__(self):
+    def __init__(self) -> None:
         self.translated_sentence = ""
+        self.sentence = self.get_string_to_encrypt().upper()
 
+    def get_string_to_encrypt(self) -> str:
+        """Get form user string to encrypting"""
+        return input("Input sentence to encrypting: ")
 
