162e6aaa7b97af849e935f1b64e92aa6da4fc278
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-08-15 19:42:34 +0200

    feat: add method encrypting_text

--- class_Encrypter.py
+++ class_Encrypter.py
@@ -1,9 +1,13 @@
+LAST_LETTER = 90
+OFFSET = 26
+
+
 class Encrypter:
     """Represents a class used to encrypt a file using the Caesar cipher  mod47 or mod13"""
     def __init__(self) -> None:
-        self.translated_sentence = ""
         self.sentence = self.get_string_to_encrypt().upper()
         self.key = self.get_key()
+        self.translated_sentence = self.encytping_text()
 
     def get_string_to_encrypt(self) -> str:
         """Get form user string to encrypting"""
@@ -13,4 +17,13 @@ class Encrypter:
         """Get from user key to encrypting"""
         return int(input("Input key to encrypting: "))
 
+    def encytping_text(self) -> str:
+        encrypted_txt = ""
 
+        for letter in self.sentence:
+            if ord(letter) + self.key <= LAST_LETTER:
+                encrypted_txt += chr(ord(letter) + self.key)
+            else:
+                encrypted_txt += chr(ord(letter) + self.key - OFFSET)
+
+        return encrypted_txt
