bcd3f753655e5c4b525611a54a137f123f2fbb9f
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-08-17 21:04:34 +0200

    feat: add method crate_content in class FileReader

--- class_File_Reader.py
+++ class_File_Reader.py
@@ -7,10 +7,25 @@ class FileReader:
         self.results = results
         self.time = time.strftime("%H-%M_%d-%m-%Y", time.localtime())
         self.file_name = self.create_file_name()
+        self.content = self.create_content()
 
     def create_file_name(self) -> str:
         """Create and return file name"""
         return f"Results_{self.time}"
 
+    def create_content(self) -> str:
+        """Create and return the content to save"""
+        content = ""
+
+        if not self.results:
+            return "No data in memory"
+
+        for result in self.results:
+            for key, value in zip(result.keys(), result.values()):
+                content += f"{key}: {value}\t",
+
+            content += "\n"
+
+        return content
 
 
