9f8435d1fa5537b1eb905139709a59f9f6c05d54
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-08-17 20:54:33 +0200

    feat: add method create_file_name to class FileReader

--- class_File_Reader.py
+++ class_File_Reader.py
@@ -6,3 +6,11 @@ class FileReader:
     def __init__(self, results: list) -> None:
         self.results = results
         self.time = time.strftime("%H-%M_%d-%m-%Y", time.localtime())
+        self.file_name = self.create_file_name()
+
+    def create_file_name(self) -> str:
+        """Create and return file name"""
+        return f"Results_{self.time}"
+
+
+
